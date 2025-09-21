936ae8639a184f655c7060ff9c117813ebe2f340
konopeito <juliafritsch04@gmail.com>
2025-09-21 16:05:57 -0500

    Added new assets, Added animation to new items and a collection slider

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, IItem
 {
+    public static event Action<int> OnCoinCollect;
+    public int worth = 5;
     public void Collect()
     {
+        OnCoinCollect.Invoke(worth);
         Destroy(gameObject);
     }
 
